ff86bba5139b008e9837297fca952bb1be5b4693
Antony Alfredo Culqui Carranza <antonyalfredoculquicarranza@MacBook-Pro-de-Antony.local>
2024-01-13 23:49:05 -0500

    Inicio Proyecti

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '2kb' }))
+
+var corsOptions = {
+    origin: "*",
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+app.get('/', function(req, res) {
+    res.send('Hola Mundo')
+})
+
+const port = process.env.PORT = 3000
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
